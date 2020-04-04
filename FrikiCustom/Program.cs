1978017f0bf8a7761fc3ce9468bc89f36a08da90
Nico <Nico@DESKTOP-VPR09JL>
2020-04-04 12:32:17 +0200

    Start commit

--- FrikiCustom/Program.cs
+++ FrikiCustom/Program.cs
@@ -20,7 +20,7 @@ namespace FrikiCustom
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); 
                 });
     }
 }
